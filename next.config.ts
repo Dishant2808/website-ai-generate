87267593ea6bc265563c67f6814a538141ebe33d
gaurav-2k13 <gaurav@hashsoftware.com>
2026-07-08 16:23:55 +0530

    fixed preview

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["playwright-core", "@sparticuz/chromium", "playwright"],
   images: {
     remotePatterns: [
       {
@@ -11,6 +12,10 @@ const nextConfig: NextConfig = {
   },
   outputFileTracingIncludes: {
     "/api/export/[id]": [
+      "./node_modules/playwright-core/**/*",
+      "./node_modules/@sparticuz/chromium/**/*",
+    ],
+    "/api/export/[id]/route": [
       "./node_modules/playwright-core/**/*",
       "./node_modules/@sparticuz/chromium/**/*",
     ],
